449dc74408f70d4e5b3d617893628fae1b8cc15c
WorldSolutionRdc <worldsolutiontv@gmail.com>
2026-04-17 10:55:52 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Démarrer badvpn-udpgw (proxy UDP)
 tmux new-session -d -s a0 'badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 250 --max-connections-for-client 3'
+
+# Démarrer dropbear (serveur SSH)
 tmux new-session -d -s b0 'dropbear -REF -p 40000 -W 65535'
 
+# Démarrer nginx (load balancer multi-backend)
+nginx -g "daemon off;" &
+
+# Démarrer proxy3.js (WebSocket vers nginx local)
 export PORT=${PORT:-8080}
+export DHOST="127.0.0.1"
+export DPORT="8080"
 node proxy3.js
